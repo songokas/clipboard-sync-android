97cd5d61b5ed412474fa9eb10494af56116639a9
Tomas Jakstas <tom.jakstas@gmail.com>
2020-12-25 02:39:26 +0200

    initial commit

---
+++ app/src/main/java/com/clipboard/sync/ClipboardSync.kt
@@ -0,0 +1,16 @@
+package com.clipboard.sync
+
+class ClipboardSync {
+
+    companion object {
+        init {
+            System.loadLibrary("clipboard_sync")
+        }
+    }
+
+    external fun startSync(config: String): String;
+
+    external fun stopSync(): String;
+
+    external fun status(): String;
+}
---
+++ app/src/main/java/com/clipboard/sync/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.clipboard.sync
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_settings) {
+            // launch settings activity
+            startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
