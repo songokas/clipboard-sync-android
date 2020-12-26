7b172d6d020f3cfd001458e63d7b103f2878431e
Tomas Jakstas <tom.jakstas@gmail.com>
2020-12-26 14:20:02 +0200

    show status

--- app/src/main/java/com/clipboard/sync/MainFragment.kt
+++ app/src/main/java/com/clipboard/sync/MainFragment.kt
@@ -15,6 +15,7 @@ import androidx.fragment.app.Fragment
 class MainFragment : Fragment() {
     private lateinit var textView: TextView
     private lateinit var toggleButton: SwitchCompat
+    private lateinit var timerHandler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,34 +27,39 @@ class MainFragment : Fragment() {
         outState.putBoolean("statusText", toggleButton.isChecked)
     }
 
-
-
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
 
+        timerHandler = Handler(Looper.getMainLooper())
         val view = inflater.inflate(R.layout.main_fragment, container, false)
         textView = view.findViewById(R.id.text_view)
         toggleButton = view.findViewById(R.id.toggle_button)
         val mainActivity = activity as MainActivity
 
         if (savedInstanceState != null) {
-
             textView.text = savedInstanceState.getString("statusText")
             toggleButton.isChecked = savedInstanceState.getBoolean("buttonState", false)
-
-            val timerHandler = Handler(Looper.getMainLooper())
-            object : Runnable {
-                override fun run() {
-                    textView.text = mainActivity.getStatus()
-                    timerHandler.postDelayed(this, 6000)
-                }
-            }
         }
+
         toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                startLog()
+            }
             mainActivity.changeState(textView, isChecked)
         }
         return view
+    }
+
+    private fun startLog()
+    {
+        val runnable = object : Runnable {
+            override fun run() {
+                textView.text = (activity as MainActivity).getStatus()
+                timerHandler.postDelayed(this, 6000)
+            }
+        }
+        timerHandler.postDelayed(runnable, 6000)
     }
 }
